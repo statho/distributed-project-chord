448357fa1e042ffcbe9dbcaf237f0b0843f51f41
Anastasis Stathopoulos <anas.stathop@gmail.com>
2017-03-16 22:06:03 +0200

    Report stuff

--- Makefile
+++ Makefile
@@ -22,4 +22,7 @@ run:
 	/usr/lib/erlang/bin/erl -noshell -s test extreme_depart_test -s erlang halt
 
 testcase1:
-	/usr/lib/erlang/bin/erl -noshell -s testcase1 main3 eventual 3 -s erlang halt		
+	/usr/lib/erlang/bin/erl -noshell -s testcases main3 eventual 3 -s erlang halt
+
+clean:
+	rm *.beam
--- run_test1.sh
+++ run_test1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "Eventual 1"
-/usr/lib/erlang/bin/erl -noshell -s testcase1 main eventual 1 -s erlang halt
+/usr/lib/erlang/bin/erl -noshell -s testcases main eventual 1 -s erlang halt
 
 echo "Eventual 3"
-/usr/lib/erlang/bin/erl -noshell -s testcase1 main eventual 3 -s erlang halt
+/usr/lib/erlang/bin/erl -noshell -s testcases main eventual 3 -s erlang halt
 
 echo "Eventual 5"
-/usr/lib/erlang/bin/erl -noshell -s testcase1 main eventual 5 -s erlang halt
+/usr/lib/erlang/bin/erl -noshell -s testcases main eventual 5 -s erlang halt
 
 echo "Linearizability 1"
-/usr/lib/erlang/bin/erl -noshell -s testcase1 main linearizability 1 -s erlang halt
+/usr/lib/erlang/bin/erl -noshell -s testcases main linearizability 1 -s erlang halt
 
 echo "Linearizability 3"
-/usr/lib/erlang/bin/erl -noshell -s testcase1 main linearizability 3 -s erlang halt
+/usr/lib/erlang/bin/erl -noshell -s testcases main linearizability 3 -s erlang halt
 
 echo "Linearizability 5"
-/usr/lib/erlang/bin/erl -noshell -s testcase1 main linearizability 5 -s erlang halt
+/usr/lib/erlang/bin/erl -noshell -s testcases main linearizability 5 -s erlang halt
